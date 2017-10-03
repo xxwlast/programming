3cf1da666dbf08986a11ef0c4e8324f37b7107dc
xxwlast <31654837+xxwlast@users.noreply.github.com>
2017-10-03 11:54:42 +0300

    Add files via upload
    В этот раз все правильно

---
+++ pp.py
@@ -0,0 +1,53 @@
+#word = "Добрый_день"
+#print(word)
+
+#part = word[3:6]
+#print(word[:3] + '!' + part +'!'+ word[6:])
+#print()
+
+#my_sum = 0
+#for i in range(3, 20, 2):
+  #  my_sum += i
+ #   print(i, "my_sum =", my_sum)
+    #if i > 10 and i % 2 != 0:
+     #   print(i**2)
+#print(my_sum)
+#print('End')
+
+#num = int(input("Введите число: ")
+#my_sum = 0
+          #for k in range(num+1):
+          #my_sum += k
+          #print(k, "my_sum =", my_sum)
+          
+#i = 10
+#while i >= 5:
+   # print(i)
+   # i -=1
+#print("i =", i)
+
+#for i in range(10):
+   # print(i)
+  #  if i**2 > 100:
+ #       break
+#print('end')
+#i = 2
+#while i**2 <= 100:
+  #  print(i, i**2)
+ #   i +=1
+#print("i =", i)
+
+#print("hello", end=', ')
+#print("world")
+
+#for i in range(10):
+ #   print()
+  #  print(i, end=', ')
+   # if i== 2:
+    #    continue
+    #print(i**2)
+#print('end')
+
+for i in range(5):
+    word = input("Ведите слово; ")
+    print(len(word))
