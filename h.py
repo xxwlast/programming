c77eed5348558de60da2f7b9d85d8fb86f758bd3
xxwlast <31654837+xxwlast@users.noreply.github.com>
2017-11-30 11:40:05 +0300

    Add files via upload

---
+++ h.py
@@ -0,0 +1,87 @@
+#def hello(user):
+#    print("Hello, " + user + "!")
+    
+#hello("Polya")
+
+#def - создает функцию
+#user = Polya
+#hello("username")
+
+#def hello(user1, user2, user3):
+#    print("Hello, " + user1 + "!")
+#    print(user2.title())
+#    print(user3. title())
+    
+#hello("Polya", "Masha,", "Misha")
+
+
+#def hello(user1, age):
+    #print("Hello, " + user1 + "!")
+    #if age > 10:
+     #   print("Older than 10")
+    #else:
+    #    print(age)
+   # sum_ = 0
+  #  for i in range(10):
+    #sum_ += 1
+    #print(sum_)
+    #print(gl)
+ #   new_name = user1.title
+#    return new_name
+
+#user_title = hello('polya', 17)
+#print(user_title)
+
+#gl = 100   
+#hello("Polya", 17)
+#print(sum_)
+
+
+#def hello(user1):
+#    print("Hello, " + user1 + "!")
+#    return user1.title()
+#user_title = hello("polya")
+#print(user_title)
+
+#def count(word):
+ #   for l in word:
+ #       print(l)
+ #   print(len(word))
+#sent = 'please kill me'
+#els = sent.split()
+#count(els)
+
+#def elements(word):
+ #   ''''''
+ #   for l in word:
+ #       print(l)
+ #       print(len(word)) 
+
+#def tokenize(sentence):
+  #  words = sentence.split()
+ #   return words
+#sent = 'i beg to dream and differ' 
+
+#els=sent.split() 
+#elements(els) 
+
+
+
+
+
+
+for w in tok_result:
+    print(w.upper())
+
+def lines_div(text.txt):
+    with open(text.txt, encoding = 'utf-8' as
+              lines_raw = f.readlines()
+    line_lengths = []
+    fot lines in lines_raw:
+        clear_line = line.strip()
+        if clear_line:
+              lines_length.append(len(clear_line))
+
+    return min(lines_length)
+
+min_l = lines.div
